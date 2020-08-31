34590e91b343f3bb5ba0944ed09ee4d72288f650
Sandra <oawx72@gmail.com>
2020-08-31 14:32:50 +0200

    app avant correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import * as axios from "axios";
+
+
 
 function App() {
+    const [number, setNumber] = useState("")
+    const [result, setResult] = useState("")
+    const [resultPlanet, setResultPlanet] = useState("")
+
+    function getPeople(event){
+        event.preventDefault();
+        console.log(number);
+        axios.get("https://swapi.dev/api/people/" + number)
+            .then(response=>{
+                setResult(response.data);
+                console.log(response.data)
+                getPlanet(response.data)
+            })
+
+    }
+
+    function getPlanet(result) {
+        if (result) {
+            axios.get(result.homeworld)
+                .then(response=> {
+                    setResultPlanet(response.data)
+                    console.log(response.data)
+                })
+        }
+    }
+
+    function displayPlanets() {
+        return <p>{resultPlanet.name}</p>
+    }
+
+    function displayPeople() {
+        return <p>{result.name}</p>
+    }
+
+    function updateNumber(event) {
+        setNumber(event.target.value)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={getPeople}>
+            <input type="text" placeholder="number" onChange={updateNumber}/>
+            <button type="submit" value="valider">Valider</button>
+        </form>
+        {displayPeople()}
+        {displayPlanets()}
     </div>
   );
 }
